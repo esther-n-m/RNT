2a5434a07c9ff10cc9ff6d4fb122cbff2dfa4a24
esther-n-m <esthermutembei50@gmail.com>
2026-01-31 21:38:52 +0300

    created a script.js file and created a products array of objects in it

---
+++ frontend_RNT/javascript/script.js
@@ -0,0 +1,70 @@
+console.log("JS is connected")
+
+const products = [
+    {
+        name: "Vanilla Scented Candle",
+        description: "A soft vanilla scent that creates a calm, cozy atmosphere for evenings, self-care moments and unwinding after a long day.",
+        price: 1200
+    },
+
+    {
+        name: "Citrus Scented Candle",
+        description: "A fresh citrus blend that energizes your space and helps you feel more focused and refreshed.",
+        price:  1600
+    },
+
+    {
+        name: "Pine & Cinnamon Scented Candle",
+        description: "A warm, woody scent with a hint of spice, perfect for creating a cozy, comforting vibe on cold or rainy days.",
+        price: 1900
+    },
+
+    {
+        name: "Coconut & Sandalwood Scented Candle",
+        description: "A rich, luxurious blend that makes your space feel premium, relaxing, and thoughtfully styled.",
+        price: 2500
+    },
+
+    {
+        name: "Lavender Scented Candle",
+        description: "A gentle lavender aroma designed to reduce stress and help you relax before bed.",
+        price: 1300
+    },
+
+    {
+        name: "Soft Throw Pillows",
+        description: "A simple, comfortable pillow that adds softness and warmth to your living room or bedroom.",
+        price: 2000
+    },
+
+    {
+        name: "Knot Pillow",
+        description: "A modern accent pillow that adds character and comfort to your space without overwhelming your design.",
+        price: 2100
+    },
+
+    {
+        name: "Sausage Pillow",
+        description: "A long, supportive cushion that combines comfort with a stylish, hotel-like feel.",
+        price: 2200
+    },
+
+    {
+        name: "Round Pillow",
+        description: "A plush round pillow that adds a cozy, relaxed touch to sofas, chairs, and reading corners.",
+        price: 2300
+    },
+
+    {
+        name: "Fringe Pillow",
+        description: "A decorative pillow with soft fringe details that brings texture and elegance to any room.",
+        price: 2400
+    },
+
+    {
+        name: "Geometric Pillow",
+        description: "A modern patterned pillow that adds visual interest and a clean, contemporary look to your space.",
+        price: 2500
+    }
+
+];
