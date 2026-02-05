5d8ff9b51b8f166090c863f599cbc4d904f3710e
esther-n-m <esthermutembei50@gmail.com>
2026-02-05 22:40:38 +0300

    update product object paths to include image assets

--- frontend_RNT/javascript/script.js
+++ frontend_RNT/javascript/script.js
@@ -6,67 +6,78 @@ const products = [
     {
         name: "Vanilla Scented Candle",
         description: "A soft vanilla scent that creates a calm, cozy atmosphere for evenings, self-care moments and unwinding after a long day.",
-        price: 1200
+        price: 1200,
+        image: "../images/Vamilla_Scented_Candle.png"
     },
 
     {
         name: "Citrus Scented Candle",
         description: "A fresh citrus blend that energizes your space and helps you feel more focused and refreshed.",
-        price:  1600
+        price:  1600,
+        image: "../images/Citrus_Scented_Canle.png"
     },
 
     {
         name: "Pine & Cinnamon Scented Candle",
         description: "A warm, woody scent with a hint of spice, perfect for creating a cozy, comforting vibe on cold or rainy days.",
-        price: 1900
+        price: 1900,
+        image: "../images/Pine_&_Cinnamon_Scented_Candle.png"
     },
 
     {
         name: "Coconut & Sandalwood Scented Candle",
         description: "A rich, luxurious blend that makes your space feel premium, relaxing, and thoughtfully styled.",
-        price: 2500
+        price: 2500,
+        image:"../images/Coconut_&_Sandalwood_Scented_Candle.png"
     },
 
     {
         name: "Lavender Scented Candle",
         description: "A gentle lavender aroma designed to reduce stress and help you relax before bed.",
-        price: 1300
+        price: 1300,
+        image: "../images/Lavender_Scented_Candle.png"
     },
 
     {
         name: "Soft Throw Pillows",
         description: "A simple, comfortable pillow that adds softness and warmth to your living room or bedroom.",
-        price: 2000
+        price: 2000,
+        image: "../images/Soft_Throw_Pillow.png"
     },
 
     {
         name: "Knot Pillow",
         description: "A modern accent pillow that adds character and comfort to your space without overwhelming your design.",
-        price: 2100
+        price: 2100,
+        image: "../images/Knot_Pillow.png"
     },
 
     {
         name: "Sausage Pillow",
         description: "A long, supportive cushion that combines comfort with a stylish, hotel-like feel.",
-        price: 2200
+        price: 2200,
+        image: "../images/Sausage_Pillow.png"
     },
 
     {
         name: "Round Pillow",
         description: "A plush round pillow that adds a cozy, relaxed touch to sofas, chairs, and reading corners.",
-        price: 2300
+        price: 2300,
+        image: "../images/Round_Pillow.png"
     },
 
     {
         name: "Fringe Pillow",
         description: "A decorative pillow with soft fringe details that brings texture and elegance to any room.",
-        price: 2400
+        price: 2400,
+        image: "../images/Fringe_Pillow.png"
     },
 
     {
         name: "Geometric Pillow",
         description: "A modern patterned pillow that adds visual interest and a clean, contemporary look to your space.",
-        price: 2500
+        price: 2500,
+        image: "../images/Geometric_Pillow.png"
     }
 
 ];
